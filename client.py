f33a1e5baaacd825a0c9ba7da458e8787b7a4417
baumanproject <ledgergap@gmail.com>
2020-09-15 21:38:42 +0300

    client works with hard code

--- client.py
+++ client.py
@@ -8,18 +8,20 @@ from clickhouse_driver import Client
 import sys
 import logging
 
+
+
+
+def json_iter(json_file_path):
+    with open(json_file_path) as reader:
+        for line in reader:
+            line = json.loads(line)
+            list_check = {"ts":0,"userId":"","sessionId":0,"page":"","auth":"","method":"","status":0,"level":"","itemInSession":0,"location":"","userAgent":"","lastName":"","firstName":"","registration":0,"gender":"","artist":"","song":"","length":0.0}
+            lack = list(set(line.keys())^set(list_check.keys()))
+            for i in lack:
+                line[i] = list_check[i]
+            yield line
+
 def startTable(client):
-    json_data = []
-    json_file = '/home/aquafeet/dep/event-data.json'
-    file = open(json_file)
-    #with open(json_file, 'r') as file:
-     #   data = file.read().replace('\n', ' ')
-    #print(data)'''
-
-    for line in file:
-        json_line = json.loads(line)
-        json_data.append(json_line)
-
 
 
     client.execute("CREATE TABLE blumb ("
@@ -29,23 +31,22 @@ def startTable(client):
                    "page String,"
                    "auth String,"
                    "method String,"
-                   "status Int8,"
+                   "status Int64,"
                    "level String,"
-                   "itemInSession Int8,"
+                   "itemInSession Int64,"
                    "location String,"
                    "userAgent String,"
                    "lastName String,"
                    "firstName String,"
                    "registration Int64,"
                    "gender String,"
-                   "artist String,"
-                   "song String,"
-                   "length Float32"
+                   "artist String DEFAULT toString('no'),"
+                   "song String DEFAULT toString('no'),"
+                   "length Float64 DEFAULT toFloat64(0)"
                    ")"
                    "ENGINE = Log()"
-                   
-                   "SETTINGS index_granularity = 8192")
-    return json_data
+                   )
+
 
 
 # Press the green button in the gutter to run the script.
@@ -53,29 +54,22 @@ if __name__ == '__main__':
 
     client = Client('localhost')
 
-    json_data = 0
 
     client.execute('DROP TABLE IF EXISTS blumb')
-    json_data = startTable(client)
-
-
-    print(json_data[0:5])
+    startTable(client)
     print("Write json")
-    print(client.execute("SELECT * FROM system.settings WHERE name IN('max_execution_time', 'receive_timeout')"))
+    json_file = '/home/aquafeet/dep/event-data.json'
 
-
-    sets = {'input_format_skip_unknown_fields': 1, 'max_insert_block_size':100}
-    client.execute_with_progress("INSERT INTO blumb FORMAT JSON", json_data[0:5], with_column_types=False, settings=sets,
-                   types_check=False)
-
-
-
-    print(client.execute("SELECT COUNT(*) FROM blumb"))
+    sets = {"input_format_null_as_default":1,"input_format_values_interpret_expressions" :1}
+    client.execute("INSERT INTO blumb VALUES", (line for line in json_iter(json_file)), with_column_types=True, settings=sets, types_check=False)
+    print("Enter")
+    a = (client.execute("SELECT * FROM blumb",columnar = True,with_column_types=True))
     print("Lines inside table")
-    print(client.execute(" SELECT gender, lastName  FROM blumb WHERE gender = 'M' "))
+    print(client.execute("SELECT COUNT(song), count(artist) FROM blumb GROUP BY song", columnar = True))
+   # print(client.execute(" SELECT gender, lastName  FROM blumb WHERE gender = 'M' "))
 
-
-    '''client.execute('SHOW TABLES')
+    '''
+    client.execute('SHOW TABLES')
     client.execute('DROP TABLE IF EXISTS test')
     client.execute('CREATE TABLE test (x Int32) ENGINE = Memory')
     client.execute('INSERT INTO test (x) VALUES', [{'x': 100}])
@@ -83,9 +77,14 @@ if __name__ == '__main__':
     client.execute('INSERT INTO test (x) SELECT * FROM system.numbers LIMIT %(limit)s', {'limit': 3})
 
     logging.warning(client.execute('SELECT sum(x) FROM test'))
-    logging.warning('All Good\n')'''
+    logging.warning('All Good\n')
 
+    a = (client.execute("SELECT * FROM test", columnar=True, with_column_types=True))
 
+    print(a)
+
+    print(client.execute("DESC TABLE test", columnar = False))
+'''
 
 
 
